1a65f8ac6d8270d054a004c6102eb24f614a8946
Baremexibear <migueljr12321@yahoo.com>
2026-01-23 19:16:51 -0500

    Implement LFO oscillator for tremolo effect with sine wave modulation

--- todo/tremolo_plugin/include/Tremolo/Tremolo.h
+++ todo/tremolo_plugin/include/Tremolo/Tremolo.h
@@ -1,17 +1,26 @@
 #pragma once
 
+#include "juce_dsp/juce_dsp.h"
 namespace tremolo {
 class Tremolo {
 public:
+  tremolo(){
+    lfo.setFrequency(5.f /* Hz*/, true);
+  }
   void prepare(double sampleRate, int expectedMaxFramesPerBlock) {
-    juce::ignoreUnused(sampleRate, expectedMaxFramesPerBlock);
+    const juce::dsp::ProcessSpec processSpec {
+      .sampleRate = sampleRate,
+      .maximumBlockSize = static_cast<juce::unit32>(expectedMaxFramesPerBlock),
+      .getNumChannels = 1u,
+    };
+    lfo.prepare(processSpec);
   }
 
   void process(juce::AudioBuffer<float>& buffer) noexcept {
     // for each frame
     for (const auto frameIndex : std::views::iota(0, buffer.getNumSamples())) {
-      // TODO: generate the LFO value
-
+      //  generate the LFO value
+      const auto lfoValue = lfo.processSample(0.f);
       // TODO: calculate the modulation value
 
       // for each channel sample in the frame
@@ -29,9 +38,12 @@ public:
     }
   }
 
-  void reset() noexcept {}
+  void reset() noexcept {
+    lfo.reset()
+  }
 
 private:
   // You should put class members and private functions here
+  juce::dsp::Oscillator<float>lfo;{[](auto phase){return std::sin(phase);}}
 };
 }  // namespace tremolo
